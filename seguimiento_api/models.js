6c93dc31b6cce401e572fbf53e64f2e272c4bc34
fridaluz22 <fridaluzf14@gmail.com>
2024-06-11 14:48:29 -0500

    File internadoApi and User API

--- seguimiento_api/models.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const { seguimientoSchema } = require('./schemas');
-
-const seguimientoModel = mongoose.model('seguimiento', seguimientoSchema);
-
-module.exports = {seguimientoModel };
